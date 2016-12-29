dd6dc882e626a2f18cea1b835c6484a004243d31
Konstantin <2447785@gmail.com>
2016-12-29 17:28:38 +0300

    Ajax booking system

--- web/controllers/booking_controller.ex
+++ web/controllers/booking_controller.ex
@@ -5,34 +5,116 @@ defmodule ConcertBooking.BookingController do
   alias ConcertBooking.Concert
 
 
-  def book(conn, params), do: book_handle(conn, params, "create")
+  @doc"""
+  Main book func
+  """
+  def book(conn, %{"concert_id" => concert_id})
+  when not is_nil(concert_id) do
+    book_handle(conn, concert_id, "create")
+  end
 
-  def unbook(conn, params), do: book_handle(conn, params, "delete")
+  def book(conn, _) do
+    failed_booking(conn)
+  end
 
-  defp book_handle(conn, %{"concert_id" => concert_id}, action)
-  when not is_nil(concert_id) and not is_nil(action) do
+
+  @doc"""
+  Main unbook func
+  """
+  def unbook(conn, %{"concert_id" => concert_id})
+  when not is_nil(concert_id) do
+    book_handle(conn, concert_id, "delete")
+
+  end
+
+  def unbook(conn, _) do
+    failed_booking(conn)
+  end
+
+
+  @doc"""
+  API func for book
+  """
+  def book_api(conn, %{"json" => json}) do
+    book_api(conn, Poison.Parser.parse!(json))
+  end
+
+  def book_api(conn, %{"concert_id" => concert_id})
+  when not is_nil(concert_id) do
+    book_handle_api(conn, concert_id, "create")
+  end
+
+  def book_api(conn, _) do
+    failed_booking_api(conn)
+  end
+
+
+  @doc"""
+  API func for unbook
+  """
+  def unbook_api(conn, %{"json" => json}) do
+    unbook_api(conn, Poison.Parser.parse!(json))
+  end
+
+  def unbook_api(conn, %{"concert_id" => concert_id})
+  when not is_nil(concert_id) do
+    book_handle_api(conn, concert_id, "delete")
+  end
+
+  def unbook_api(conn, _) do
+    failed_booking_api(conn)
+  end
+
+
+  @doc"""
+  Main (un)book request handle
+  """
+  defp book_handle(conn, concert_id, action) do
     concert = Repo.get!(Concert, concert_id)
 
     case Guardian.Plug.current_resource(conn) do
       nil  -> failed_booking(conn)
-      user -> update_booking(concert, user, action)
+      user ->
+        case update_booking(concert, user, action) do
+          "create" ->
+            conn
+            |> put_flash(:info, "You've booked a ticket")
+            |> redirect(to: concert_path(conn, :show, concert.id))
+
+          "delete" ->
+            conn
+            |> put_flash(:info, "You've unbooked a ticket")
+            |> redirect(to: concert_path(conn, :show, concert.id))
+        end
     end
-
-    conn
-    |> put_flash(:info, "You've booked a ticket")
-    |> redirect(to: concert_path(conn, :show, concert.id))
   end
 
-  defp book_handle(conn, _, _) do
-    failed_booking(conn)
+  @doc"""
+  API (un)book request handle
+  """
+  defp book_handle_api(conn, concert_id, action) do
+    concert = Repo.get!(Concert, concert_id)
+
+    case Guardian.Plug.current_resource(conn) do
+      nil  -> failed_booking_api(conn)
+      user ->
+        action = update_booking(concert, user, action)
+        [[count]] = Ecto.Adapters.SQL.query!(Repo, "SELECT COUNT(user_id) FROM users_concerts", []).rows
+        json conn, %{ "action" => action, "count" => count }
+    end
   end
 
+
+  @doc"""
+  Func for working with db
+  """
   defp update_booking(concert, user, action) do
     case action do
       "create" ->
         Ecto.Adapters.SQL.query!(Repo,
           "INSERT INTO users_concerts (user_id, concert_id) VALUES ($1, $2)",
-          [user.id, concert.id])
+          [user.id, concert.id]
+        )
         # concert
         # |> Repo.preload(:users)
         # |> Ecto.Changeset.change()
@@ -42,6 +124,7 @@ defmodule ConcertBooking.BookingController do
       "delete" ->
         Repo.delete_all( from uc in "users_concerts", where: (uc.user_id == ^user.id) and (uc.concert_id == ^concert.id))
     end
+    action
   end
 
   defp failed_booking(conn) do
@@ -49,5 +132,9 @@ defmodule ConcertBooking.BookingController do
     |> put_flash(:error, "Something got wrong")
     |> redirect(to: concert_path(conn, :index))
     |> halt()
+  end
+
+  defp failed_booking_api(conn) do
+    json conn, %{ "code" => "403", "message" => "Error..." }
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -1,24 +1,27 @@
 defmodule ConcertBooking.Router do
   use ConcertBooking.Web, :router
 
+  pipeline :session do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+    plug ConcertBooking.CurrentUser
+  end
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-
-    plug Guardian.Plug.VerifySession
-    plug Guardian.Plug.LoadResource
-    plug ConcertBooking.CurrentUser
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_session
   end
 
   scope "/", ConcertBooking do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :session]
 
     get "/", PageController, :index
     resources "/users", UserController
@@ -29,10 +32,12 @@ defmodule ConcertBooking.Router do
     post "/unbook", BookingController, :unbook
   end
 
-  # Other scopes may use custom stacks.
   scope "/api", ConcertBooking do
-    pipe_through :api
+    pipe_through [:api, :session]
 
     get "/concerts", ConcertController, :index_api
+
+    post "/book", BookingController, :book_api
+    post "/unbook", BookingController, :unbook_api
   end
 end
